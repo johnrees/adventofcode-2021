ab05ed0fc02572f12c64f45e972a891d1d6570fc
John Rees <john@bitsushi.com>
2021-12-01 21:52:17 +0000

    elixir

---
+++ 01/index.ex
@@ -0,0 +1,32 @@
+defmodule Day01 do
+  def one(numbers) do
+    numbers
+    |> Enum.reduce(%{prev: :infinity, count: 0}, fn curr, acc ->
+      %{
+        prev: curr,
+        count:
+          if(
+            curr > acc.prev,
+            do: acc.count + 1,
+            else: acc.count
+          )
+      }
+    end)
+    |> Map.get(:count)
+  end
+
+  def two(numbers) do
+    numbers
+    |> Enum.chunk_every(3, 1)
+    |> Enum.map(&Enum.sum/1)
+    |> one()
+  end
+end
+
+numbers =
+  File.read!("input.txt")
+  |> String.split("\n", trim: true)
+  |> Enum.map(&String.to_integer/1)
+
+IO.inspect(Day01.one(numbers))
+IO.inspect(Day01.two(numbers))
